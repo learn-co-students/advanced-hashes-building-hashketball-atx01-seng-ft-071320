409be58abefca0678125e995286cbd69776b1ab7
Cedric Patton <ceddyp117@gmail.com>
2020-07-04 22:23:09 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,51 @@
 # Write your code here!
+def game_hash 
+  { 
+    home:{
+      team_name: "Brooklyn Nets",
+      colors:["Black","White"], 
+      players:[
+      {:player_name => ,
+       :number => , 
+       :shoe => , 
+       :points => ,
+       :rebounds => ,
+       :assists => ,
+       :steals => ,
+       :blocks => , 
+       :slam_dunks =>
+      },
+      {}
+      
+      
+      
+      
+      
+      
+      
+      
+  ]},
+    
+    
+    
+    
+    away:{
+      team_name:"",
+      colors: ["",""],
+      players:[{
+        :player_name => ,
+       :number => , 
+       :shoe => , 
+       :points => ,
+       :rebounds => ,
+       :assists => ,
+       :steals => ,
+       :blocks => , 
+       :slam_dunks =>
+      }, 
+      {}
+      
+      
+    ]}
+  }
+end 
